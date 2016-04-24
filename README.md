50fb9fec96a7a070362cc458437ae6507c781fff
Guy Cole <guycole@gmail.com>
2016-04-23 23:39:36 -0700

    prepare to share

--- README.md
+++ README.md
@@ -1,2 +1,36 @@
 # calc-demo-aws
-AWS API Gateway and Lamba (python) demonstration
+### AWS API Gateway and Lamba (python) demonstration.
+
+4 function calculator web service (API Gateway) which invokes Lambda (python) to perform operation and return results.
+---
+## Installation
+1.  Create S3 bucket to contain Lambda calculator function
+..* For simplicity, S3 bucket should reside in a region (i.e. don't pick 'US Standard') 
+2.  Place calc.py into a zipfile and copy to S3 bucket from step 1
+..* ```
+    zip lambda.zip calc.py
+    aws s3 cp lambda.zip s3://lambda.braingang.net --region us-west-2
+    ```
+3.  Use Cloud Formation to create IAM permissions and Lambda function
+..* cf.template will prompt for bucket information (from step 1) and warn about IAM permissions
+..* Output tab will contain ARN needed for API Gateway
+4.  Prepare swagger.json for API Gateway
+..* ```
+    update credentials (2 references) w/GateWayRoleArn from Cloud Formation outputs tab
+    "credentials": "GateWayRoleArn", //original
+    "credentials": "arn:aws:iam::123456789:role/calc-GateWayRole" //tweaked
+
+    update URI (2 references) w/AWS Region and LambdaCalcArn from Cloud Formation outputs tab
+    "uri": "arn:aws:apigateway:AwsRegion:lambda:path/2015-03-31/functions/LambdaCalcArn/invocations" //original
+    "credentials": "arn:aws:lambda:us-west-2:123456789:function:CalcLambdaFunction" //tweaked
+
+    save file
+    ```
+5.  Upload swagger.json as API Gateway
+6.  Installation Complete
+---
+## Operation (from AWS console)
+1.  Stub
+---
+## Operation (from curl)
+1. Stub
--- lambda/calc.py
+++ lambda/calc.py
@@ -6,21 +6,25 @@ print('Loading function')
 
 
 def lambda_handler(event, context):
-    print("Received event: " + json.dumps(event, indent=2))
-    print('-x-x-x-x-x-x-x-')
-#    print(vars(context))
-#    print('-o-o-o-o-o-o-o-')
+#    print("Received event: " + json.dumps(event, indent=2))
 
-    operator = ""
-    operand1 = ""
-    operand2 = ""
+    operator = 'bogus'
+    operand1 = "0"
+    operand2 = "0"
+    result = 0
 
-    if 'params' in event:
-        temp = event['params']
+    if 'body' in event:
         try:
-            operator = temp['path']['operator']
-            operand1 = temp['querystring']['operand1']
-            operand2 = temp['querystring']['operand2']
+            operator = event['body']['operator']
+            operand1 = event['body']['operand1']
+            operand2 = event['body']['operand2']
+        except KeyError as keyError:
+            print('key error noted')
+    else:
+        try:
+            operator = event['operator']
+            operand1 = event['operand1']
+            operand2 = event['operand2']
         except KeyError as keyError:
             print('key error noted')
 
@@ -30,10 +34,14 @@ def lambda_handler(event, context):
     print(operand2)
     print('-o-o-o-o-o-o-o-')
 
-#    operator = 'operator'
-#    operand1 = 'operand1'
-#    operand2 = 'operand2'
-    result = 'result'
+    if operator == 'add':
+        result = float(operand1) + float(operand2)
+    elif operator == 'sub':
+        result = float(operand1) - float(operand2)
+    elif operator == 'mul':
+        result = float(operand1) * float(operand2)
+    elif operator == 'div':
+        result = float(operand1) / float(operand2)
 
-    output = "{'operator':%s, 'operand1':%s, 'operand2':%s, 'result':%s}" % (operator, operand1, operand2, result)
+    output = "{'operator':%s, 'operand1':%s, 'operand2':%s, 'result':%f}" % (operator, operand1, operand2, result)
     return output
